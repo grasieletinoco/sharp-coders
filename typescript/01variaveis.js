8031b282c5686dfd9ab98a39dcd15deba96be9a5
grasieletinoco <grasieleprt@gmail.com>
2023-12-02 21:43:20 -0300

    TypeScript

---
+++ typescript/01variaveis.js
@@ -0,0 +1,11 @@
+/*
+1º Conjunto de superset para JS / Linguagem.
+2º Microsoft é criadora e mantedora do TS.
+3º Boa parte das tecnologias front-end utilizam TS.
+4º Criado pelo desenvolvedor: Anders Hejlsberg.
+*/
+//VARIÁVEIS
+var nome = 'Camila';
+var idade = 26;
+var estuda = true;
+var endereco = 'Centro';
