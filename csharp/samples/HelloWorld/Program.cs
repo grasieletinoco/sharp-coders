26b60dc9d1dae4541f5608fd01156849f2b6bbe9
grasieletinoco <grasieleprt@gmail.com>
2024-01-16 21:56:43 -0300

    C# - Aula 01

---
+++ csharp/samples/HelloWorld/Program.cs
@@ -0,0 +1,41 @@
+﻿// file-scoped namespace
+namespace HelloWorld;
+
+public class Program
+{
+
+    // Funcão Principal - Ponto de Entrada - entrypoint
+    public static void Main()
+    {
+        // variaveis
+        int a = 2;
+        long l = 43;
+        long x = 43l;
+
+        float f = 2.3f;
+        double d = 2;
+
+        long inteiroLongo = 3L;
+        int longo = (int)inteiroLongo;
+
+        string name = "Hugo Rafael";
+
+        Console.WriteLine(double.MaxValue);
+        Console.WriteLine(double.MinValue);
+
+        Console.Write("Digite Alguma Coisa para Ser Lida: ");
+        var leitura = Console.ReadLine();
+
+        Console.WriteLine(leitura);
+
+        var primeiroNumero = int.Parse(Console.ReadLine());
+        var segundoNumero = Console.ReadLine();
+
+        Console.WriteLine(primeiroNumero + segundoNumero);
+
+        // Seleção/Condicionais
+        // Repetição
+        // Funções
+    }
+
+}
