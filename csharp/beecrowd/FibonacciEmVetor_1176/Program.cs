d7ec9330ed7199bbccbfc226196844eab739b979
grasieletinoco <grasieleprt@gmail.com>
2024-01-19 17:05:32 -0300

    C# - Aula 04

---
+++ csharp/beecrowd/FibonacciEmVetor_1176/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FibonacciEmVetor_1176;
+
+public class Program
+{
+  static void Main(string[] args)
+  {
+
+    ulong[] fib = new ulong[70];
+    int nTestes = int.Parse(Console.ReadLine());
+
+    // Valores Iniciais
+    fib[0] = 0;
+    fib[1] = 1;
+
+    for (int _ = 0; _ < nTestes; _++)
+    {
+
+      int n = int.Parse(Console.ReadLine());
+
+      // TODO: Melhorar percorrendo apenas os Valores ainda nao Calculados
+      for(int i = 2; i <= n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+      }
+
+      Console.WriteLine($"Fib({n}) = {fib[n]}");
+    }
+
+  }
+}
---
+++ csharp/samples/CRUD_users/Program.cs
@@ -0,0 +1,43 @@
+﻿﻿using System.Diagnostics;
+
+namespace CRUD_users;
+
+public class Program {
+
+  static void ShowMenu() {
+    Console.WriteLine("Escolha uma opcao abaixo");
+    // 1 - Para criar um Usuario
+    // 2 - Buscar um Usuario
+    // 3 - Listar os Usuarios (sem senha se houver)
+    // 4 - Atualizar um Usuario
+    // 5 - Deletar um Usuario
+    // 0 - Para Sair
+    Console.Write("Digite a Opção Desejada: ");
+  }
+
+  static void CriarUsuario() {
+    Console.WriteLine("Usuario Criado");
+  }
+
+  public static void Main () {
+
+    int size = 1000;
+    string[] usernames = new string[size];
+    string[] emails = new string[size];
+    string[] passwords = new string[size];
+
+    ShowMenu();
+    int option = int.Parse(Console.ReadLine());
+
+    while(option != 0) {
+
+      if(option == 1) {
+        CriarUsuario();
+      }
+
+      ShowMenu();
+      option = int.Parse(Console.ReadLine());
+    }
+  }
+
+}
