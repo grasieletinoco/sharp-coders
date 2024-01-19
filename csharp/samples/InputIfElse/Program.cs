9f6a0a92011b9b8c207e3b59e8940934d1fdc07a
grasieletinoco <grasieleprt@gmail.com>
2024-01-18 22:18:29 -0300

    C# - Aula 02

---
+++ csharp/samples/InputIfElse/Program.cs
@@ -0,0 +1,33 @@
+﻿﻿namespace InputIfElse;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        string nome = Console.ReadLine();
+        int idade = int.Parse(Console.ReadLine());
+        double nota = double.Parse(Console.ReadLine());
+
+        // if braceless - sem as chaves
+        // Só vale se tiver apenas uma linha dentro
+        if(idade >= 18) 
+            Console.WriteLine("Pode Dirigir");
+        else 
+            Console.WriteLine("Nao Pode Dirigir");
+        
+        // >= 7 Aprovado
+        // >= 4 e < 7 Recuperacão
+        // < 4 Reprovado
+
+        if(nota >= 7)
+            Console.WriteLine("Aprovado");
+        else if(nota >= 4)
+            Console.WriteLine("Recuperacão");
+        else
+            Console.WriteLine("Reprovado");
+        
+
+        Console.WriteLine($"Nome: {nome}, Idade: {idade}, Nota: {nota}");
+    }
+}
