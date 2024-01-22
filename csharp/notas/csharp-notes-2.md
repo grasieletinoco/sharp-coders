39190d675f426c06142bd089e10cf12f0491c08c
grasieletinoco <grasieleprt@gmail.com>
2024-01-22 00:59:38 -0300

    C# Aula 05 - 17/01

--- csharp/notas/csharp-notes-2.md
+++ csharp/notas/csharp-notes-2.md
@@ -60,6 +60,138 @@ Menção Honrosa:
 - objetivo/resolve problemas
 - Legibilidade
 
+## Práticas interessantes
+
+### early return (retorno "adiantado")
+
+- Tento retornar antes, e mantenho um caso padrao
+
+Jeito Antigo:
+
+```cs
+static boolean EhPar(int number) {
+  if(number%2 == 0)
+    return true;
+  else
+    return false;
+}
+```
+
+Boa Prática do early return:
+
+```cs
+static boolean EhPar(int number) {
+  if(number%2 == 0)
+    return true;
+
+  return false;
+}
+```
+
+```cs
+static void Sacar(double quantia) {
+  if(ativa == true && saldo >= quantia) {
+    saldo -= quantia;
+  }
+}
+```
+
+Refatorando...
+
+```cs
+static void Sacar(double quantia) {
+  if(!ativa)
+    throw new Exception("Conta inativa");
+
+  if(saldo < quantia)
+    throw new Exception("Saldo insuficiente");
+
+  if(!cpfValido)
+    throw new Exception("Cpf Invalido");
+
+  saldo -= quantia;
+}
+```
+
+Se for Múltiplo de 3 = foo
+Se for Múltiplo de 5 = boo
+Se for Múltiplo de Ambos = fooboo
+
+```cs
+public boolean IsMultipleOf(int number, int divisor) {
+  if(number%divisor != 0)
+    return false;
+
+  return true;
+}
+
+public static string TestFooBoo(int number) {
+  string aux = "";
+
+  if(IsMultipleOf(number, 3))
+    aux += "foo";
+
+  if(IsMultipleOf(number, 5))
+    aux += "boo";
+
+  return aux;
+}
+
+Console.WriteLine(TestFooBoo(int.Parse(Console.ReadLine())));
+```
+
+Código Ruim = Dificil de extender, entender e mudar
+
+### switch-case
+
+```cs
+string sinal = "vermelho";
+
+switch(sinal) {
+  case "vermelho":
+    Console.WriteLine("Parar");
+    break;
+  case "amarelo":
+    Console.WriteLine("Atencão");
+  case "verde":
+    Console.WriteLine("Passar");
+    break;
+  default: // padrao
+    Console.WriteLine("Dirija com Cuidado");
+    break;
+}
+```
+
+```cs
+static void IsConsonant(char c) {
+  swith(c) {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+      return true;
+  }
+
+  return false;
+}
+```
+
+## Predencia do ++/--
+
+variavel++ = primeiro entrega o valor e depois aumenta
+++variavel = primeiro aumenta e depois entrega o valor
+
+```cs
+int x = 10;
+
+Console.WriteLine(++x); // 11
+Console.WriteLine(x); // 11
+
+int y = 10;
+Console.WriteLine(y++); // 10
+Console.WriteLine(y); // 11
+```
 ## Fibonacci
 
 cada próximo termo é a soma dos 2 anteriores a ele
--- csharp/samples/CRUD_users/Program.cs
+++ csharp/samples/CRUD_users/Program.cs
@@ -5,39 +5,125 @@ namespace CRUD_users;
 public class Program {
 
   static void ShowMenu() {
-    Console.WriteLine("Escolha uma opcao abaixo");
-    // 1 - Para criar um Usuario
-    // 2 - Buscar um Usuario
-    // 3 - Listar os Usuarios (sem senha se houver)
-    // 4 - Atualizar um Usuario
-    // 5 - Deletar um Usuario
-    // 0 - Para Sair
+    Console.WriteLine("Escolha uma Opção Abaixo");
+    Console.WriteLine("1 - Para Criar um Usuário");
+    Console.WriteLine("2 - Buscar um Usuário");
+    Console.WriteLine("3 - Listar os Usuários (sem senha se houver)");
+    Console.WriteLine("4 - Atualizar um Usuário");
+    Console.WriteLine("5 - Deletar um Usuário");
+    Console.WriteLine("0 - Para Sair");
     Console.Write("Digite a Opção Desejada: ");
   }
 
-  static void CriarUsuario() {
-    Console.WriteLine("Usuario Criado");
+  static void CriarUsuario(int emptyPosition, string[] usernames, string[] emails, string[] passwords) {
+    Console.Write("Digite seu Username: ");
+    usernames[emptyPosition] = Console.ReadLine();
+
+    Console.Write("Digite seu Email: ");
+    emails[emptyPosition] = Console.ReadLine();
+
+    Console.Write("Digite sua Senha: ");
+    passwords[emptyPosition] = Console.ReadLine();
   }
+
+
+
+
+  // // Teste 01
+  // static void TestaCopiaReferencia(int numeroPrimitivo){
+  //   numeroPrimitivo = 99999;
+  // }
+
+  //  // Teste 02
+  //  static void TestaCopiaReferencia2(string[] vetorDeStrings){
+  //   vetorDeStrings[0] = "hrafa";
+  // }
+
+
+
 
   public static void Main () {
 
-    int size = 1000;
-    string[] usernames = new string[size];
-    string[] emails = new string[size];
-    string[] passwords = new string[size];
+    int TOTAL_SIZE = 1000, size = 0;
 
-    ShowMenu();
-    int option = int.Parse(Console.ReadLine());
+    string[] usernames = new string[TOTAL_SIZE];
+    string[] emails = new string[TOTAL_SIZE];
+    string[] passwords = new string[TOTAL_SIZE];
 
-    while(option != 0) {
+    // CriarUsuario(emptyPosition++, usernames, emails, passwords);
+    // emptyPosition++;
 
-      if(option == 1) {
-        CriarUsuario();
-      }
+    // 6 = 0, 1, 2, 3, 4, 5, _ A posição vazia é a 6
 
+    // CriarUsuario(size++, usernames, emails, passwords);
+
+    // TODO: Reutilizar isso quando for Listar todos
+    // for(int i = 0; i < size; i++) {
+    //   Console.WriteLine("-----------------");
+    //   Console.WriteLine(usernames[i]);
+    //   Console.WriteLine(emails[i]);
+    //   Console.WriteLine(passwords[i]);
+    //   Console.WriteLine("-----------------");
+    // }
+
+
+
+
+    // // Teste 01: Não mudou o valor da variável size porque foi uma cópia
+    // TestaCopiaReferencia(size);
+    // Console.WriteLine(size);
+
+    // // Teste 02: Antes não tem nada e depois foi modificada
+    // Console.WriteLine($"Antes: {usernames[0]}");
+    // TestaCopiaReferencia2(usernames);
+    // Console.WriteLine($"Depois: {usernames[0]}");
+
+
+
+
+    // Comparativo
+    // while pra menu = mostrarMenu verifico&faço mostrarMenu ...
+    // do...while = fazendo verifica fazendo
+
+    // ShowMenu();
+    // int option = int.Parse(Console.ReadLine());
+
+    // while(option != 0) {
+
+    //   switch(option) {
+    //     case 1:
+    //       CriarUsuario(size++, usernames, emails, passwords);
+    //       break;
+    //     default:
+    //       Console.WriteLine("Digite uma opcao valida!");
+    //       break;
+    //   }
+
+    //   ShowMenu();
+    //   option = int.Parse(Console.ReadLine());
+    // }
+    
+
+    int option;
+
+    do {
       ShowMenu();
       option = int.Parse(Console.ReadLine());
-    }
+
+      switch(option) {
+        case 0:
+          Console.WriteLine("Valeu, até a próxima!");
+          break;
+        case 1:
+          CriarUsuario(size++, usernames, emails, passwords);
+          break;
+        default:
+          Console.WriteLine("Digite uma Opcão Válida!");
+          break;
+      }
+            
+    } while(option != 0);
+
   }
 
 }
